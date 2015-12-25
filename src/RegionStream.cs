ebbc8eefe8ff3043c433f93602179e00d4f4241b
skwas <skwas@skwas.net>
2015-12-25 16:20:52 +0100

    * (add): added comments

--- src/RegionStream.cs
+++ src/RegionStream.cs
@@ -45,8 +45,8 @@ namespace skwas.IO
 		/// <summary>
 		/// Moves the stream region to the new start position and length. Note that this method can only be called if the stream is in 'read' mode. A stream in write mode, cannot be moved.
 		/// </summary>
-		/// <param name="startPosition"></param>
-		/// <param name="length"></param>
+		/// <param name="startPosition">The start position in the base stream.</param>
+		/// <param name="length">The length of the region in the base stream.</param>
 		public void Move(long startPosition, long length)
 		{
 			if (!_forReading)
@@ -62,8 +62,17 @@ namespace skwas.IO
 			_baseStream.Position = startPosition;
 		}
 
+		/// <summary>
+		/// Gets the base stream.
+		/// </summary>
 		public Stream BaseStream => _baseStream;
 
+		/// <summary>
+		/// When overridden in a derived class, gets a value indicating whether the current stream supports reading.
+		/// </summary>
+		/// <returns>
+		/// true if the stream supports reading; otherwise, false.
+		/// </returns>
 		public override bool CanRead
 		{
 			get 
@@ -74,6 +83,13 @@ namespace skwas.IO
 			}
 		}
 
+
+		/// <summary>
+		/// When overridden in a derived class, gets a value indicating whether the current stream supports seeking.
+		/// </summary>
+		/// <returns>
+		/// true if the stream supports seeking; otherwise, false.
+		/// </returns>
 		public override bool CanSeek
 		{
 			get 
@@ -84,6 +100,12 @@ namespace skwas.IO
 			}
 		}
 
+		/// <summary>
+		/// When overridden in a derived class, gets a value indicating whether the current stream supports writing.
+		/// </summary>
+		/// <returns>
+		/// true if the stream supports writing; otherwise, false.
+		/// </returns>
 		public override bool CanWrite
 		{
 			get
@@ -94,11 +116,23 @@ namespace skwas.IO
 			}
 		}
 
+		/// <summary>
+		/// When overridden in a derived class, clears all buffers for this stream and causes any buffered data to be written to the underlying device.
+		/// </summary>
+		/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
 		public override void Flush()
 		{
 			_baseStream.Flush();
 		}
 
+		/// <summary>
+		/// When overridden in a derived class, gets the length in bytes of the stream.
+		/// </summary>
+		/// <returns>
+		/// A long value representing the length of the stream in bytes.
+		/// </returns>
+		/// <exception cref="T:System.NotSupportedException">A class derived from Stream does not support seeking. </exception>
+		/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
 		public override long Length
 		{
 			get
@@ -111,6 +145,15 @@ namespace skwas.IO
 			}
 		}
 
+		/// <summary>
+		/// When overridden in a derived class, gets or sets the position within the current stream.
+		/// </summary>
+		/// <returns>
+		/// The current position within the stream.
+		/// </returns>
+		/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+		/// <exception cref="T:System.NotSupportedException">The stream does not support seeking. </exception>
+		/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
 		public override long Position
 		{
 			get
@@ -128,6 +171,21 @@ namespace skwas.IO
 			}
 		}
 
+		/// <summary>
+		/// When overridden in a derived class, reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
+		/// </summary>
+		/// <returns>
+		/// The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.
+		/// </returns>
+		/// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between <paramref name="offset"/> and (<paramref name="offset"/> + <paramref name="count"/> - 1) replaced by the bytes read from the current source. </param>
+		/// <param name="offset">The zero-based byte offset in <paramref name="buffer"/> at which to begin storing the data read from the current stream. </param>
+		/// <param name="count">The maximum number of bytes to be read from the current stream. </param>
+		/// <exception cref="T:System.ArgumentException">The sum of <paramref name="offset"/> and <paramref name="count"/> is larger than the buffer length. </exception>
+		/// <exception cref="T:System.ArgumentNullException"><paramref name="buffer"/> is null. </exception>
+		/// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="offset"/> or <paramref name="count"/> is negative. </exception>
+		/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+		/// <exception cref="T:System.NotSupportedException">The stream does not support reading. </exception>
+		/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
 		public override int Read(byte[] buffer, int offset, int count)
 		{
 			if (buffer == null)
@@ -154,6 +212,17 @@ namespace skwas.IO
 			return _baseStream.Read(buffer, offset, count);
 		}
 
+		/// <summary>
+		/// When overridden in a derived class, sets the position within the current stream.
+		/// </summary>
+		/// <returns>
+		/// The new position within the current stream.
+		/// </returns>
+		/// <param name="offset">A byte offset relative to the <paramref name="origin"/> parameter. </param>
+		/// <param name="origin">A value of type <see cref="T:System.IO.SeekOrigin"/> indicating the reference point used to obtain the new position. </param>
+		/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+		/// <exception cref="T:System.NotSupportedException">The stream does not support seeking, such as if the stream is constructed from a pipe or console output. </exception>
+		/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
 		public override long Seek(long offset, SeekOrigin origin)
 		{
 			long newPosition;
@@ -191,11 +260,30 @@ namespace skwas.IO
 			return Position;
 		}
 
+		/// <summary>
+		/// Not supported.
+		/// </summary>
+		/// <param name="value">The desired length of the current stream in bytes. </param>
+		/// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+		/// <exception cref="T:System.NotSupportedException">The stream does not support both writing and seeking, such as if the stream is constructed from a pipe or console output. </exception>
+		/// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
 		public override void SetLength(long value)
 		{
 			throw new NotSupportedException("The stream region cannot be modified.");
 		}
 
+		/// <summary>
+		/// When overridden in a derived class, writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
+		/// </summary>
+		/// <param name="buffer">An array of bytes. This method copies <paramref name="count"/> bytes from <paramref name="buffer"/> to the current stream. </param>
+		/// <param name="offset">The zero-based byte offset in <paramref name="buffer"/> at which to begin copying bytes to the current stream. </param>
+		/// <param name="count">The number of bytes to be written to the current stream. </param>
+		/// <exception cref="T:System.ArgumentException">The sum of <paramref name="offset"/> and <paramref name="count"/> is greater than the buffer length.</exception>
+		/// <exception cref="T:System.ArgumentNullException"><paramref name="buffer"/> is null.</exception>
+		/// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="offset"/> or <paramref name="count"/> is negative.</exception>
+		/// <exception cref="T:System.IO.IOException">An I/O error occured, such as the specified file cannot be found.</exception>
+		/// <exception cref="T:System.NotSupportedException">The stream does not support writing.</exception>
+		/// <exception cref="T:System.ObjectDisposedException"><see cref="M:System.IO.Stream.Write(System.Byte[],System.Int32,System.Int32)"/> was called after the stream was closed.</exception>
 		public override void Write(byte[] buffer, int offset, int count)
 		{
 			if (!CanWrite)
